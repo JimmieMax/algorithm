5a32f5eccd8de22055fed6b092c52e0decce6eea
Jimmie Max <jimmiemaxgo@gmail.com>
2021-10-18 18:19:50 +0800

    max profix

--- lib/algorithm/dynamic-programming/maximun-profit/maximunProfit.spec.ts
+++ lib/algorithm/dynamic-programming/maximun-profit/maximunProfit.spec.ts
@@ -4,6 +4,7 @@ import { describe, it } from "mocha";
 
 describe("maximunProfit", () => {
   it("should maximunProfit(num[]) to maximunProfitResult", () => {
+    expect(maximunProfit([7])).to.equal(0);
     expect(maximunProfit([7,1,5,3,6,4])).to.equal(5);
     expect(maximunProfit([1,2,3,3,4])).to.equal(3);
   });
--- lib/algorithm/dynamic-programming/maximun-profit/maximunProfit.ts
+++ lib/algorithm/dynamic-programming/maximun-profit/maximunProfit.ts
@@ -1,16 +1,19 @@
 /**
- * 
+ * maximunProfit
  * @param prices 
  * @returns {number}
  */
 export default function maximunProfit(prices: number[]) {
-  let result = 0;
+  if (prices === null || prices.length < 2) {
+    return 0;
+  }
+  let maxProfit = 0;
   let minPrice = prices[0];
   for (let i = 1; i < prices.length; i++) {
     const currentPrice = prices[i];
     const profit = currentPrice - minPrice;
     if (currentPrice < minPrice) minPrice = currentPrice;
-    if (result < profit) result = profit;
+    if (maxProfit < profit) maxProfit = profit;
   }
-  return result;
+  return maxProfit;
 }
